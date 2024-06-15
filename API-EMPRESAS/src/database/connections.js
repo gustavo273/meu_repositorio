b5246924f4e08679194da3a68e68bd26161b97d8
VitorMod3sto <vitorebrunomodesto@gmail.com>
2024-06-14 22:06:00 -0300

    .

--- API-EMPRESAS/src/database/connections.js
+++ API-EMPRESAS/src/database/connections.js
@@ -6,6 +6,7 @@ const DB_PASS = process.env.DB_PASS
 const DB_HOST = process.env.DB_HOST
 const DB_NAME = process.env.DB_NAME
 
+
 function main() {
     mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`)
 
